d4109d430bf1a61e24d2c5bfe66606cfd290b5a0
Miłosz Panas <miloszpanas@gmail.com>
2019-01-27 14:46:39 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 import { About } from "./components/About";
--- src/context.js
+++ src/context.js
@@ -21,7 +21,7 @@ const reducer = (state, action) => {
       return {
         ...state,
         contacts: state.contacts.map(contact =>
-          contact.id == action.payload.id ? (contact = action.payload) : contact
+          contact.id === action.payload.id ? (contact = action.payload) : contact
         )
       };
     default:
